79447a64a9356a4504770905b42dccb28171f695
EricBarlog <ericbarlog1@gmail.com>
2022-11-02 20:02:38 -0700

    first commit

---
+++ PAB-S-01.py
@@ -0,0 +1,14 @@
+# Python Book 
+# Saying Hello World and asking user's name
+
+print('Hello World!')
+print('What is your name?')
+
+myName = input()        # Ask name
+print('It is good to meet you ' + myName + '!')
+lenAge = len(myName)
+print('The length of your name is ' + str(lenAge) + ' letters.')
+
+print('What is your age?')      # Ask age
+myAge = input()
+print('You will be ' + str(int(myAge) + 1) + ' in a year.')
